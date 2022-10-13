bf5e95cc3859413a350756a7880fd79302998da4
Matias Carrizo <matutecarrizo10@gmail.com>
2022-10-13 02:25:16 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+//Crea una lista con los siguientes elementos:
+
+//- Tu nombre (string)
+
+var nombre = "Matias" ;
+
+//- Tu apellido (string)
+
+var apellido = "Carrizo" ;
+
+//- Tu edad (number)
+
+var edad = 22 ;
+
+//- ¿Eres desarrollador de aplicaciones web? (boolean)
+
+"¿Eres desarrollador de aplicaciones web?" = true ;
+
+//- Tu fecha de nacimiento (Date)
+
+var fecha = 10-09-2000 ;
+
+//- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+var librofavorito = {
+    titulo : "Don quijote de la mancha",
+    autor : "Cervantes Saavedra",
+    fecha : 1605,
+    url : "https://es.wikipedia.org/wiki/Don_Quijote_de_la_Mancha"
+};
+
+//- Tu pasatiempos favoritos (Array)
+
+var pasatiempos = [
+    futbol,
+    pc,
+    bailar,
+    videojuegos
+];
+
+//- Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar */
+
+function miActividadFavorita(){
+    return "Videojuegos"
+};
+console.log(miActividadFavorita());
